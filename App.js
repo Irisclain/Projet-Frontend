7e61638347b33e1f7b3237d8828b7247eb118cd6
Iris <clainiris@gmail.com>
2023-07-28 11:53:49 +0200

    changement nom screen

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import HomeScreen from './screens/HomeScreen';
+import MessageScreen from './screens/MessageScreen';
 import ChatScreen from './screens/ChatScreen';
 
 const Stack = createNativeStackNavigator();
@@ -9,7 +9,7 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Message" component={MessageScreen} />
         <Stack.Screen name="Chat" component={ChatScreen} />
       </Stack.Navigator>
     </NavigationContainer>
