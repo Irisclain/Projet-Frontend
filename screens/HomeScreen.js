e47dbfa2ae85dbb76b6d56f0c7edaa5d29629c27
Iris <clainiris@gmail.com>
2023-08-01 15:22:23 +0200

    homescreen + signin presta et proprio

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,23 +1,17 @@
 import React from "react";
-import { useState } from 'react';
 import {
-  ScrollView,
   StyleSheet,
   Text,
-  TextInput,
   TouchableOpacity,
   View,
+  Image,
 } from 'react-native';
+import {LinearGradient} from 'expo-linear-gradient';
 import {
   SafeAreaView,
-  SafeAreaProvider,
-  SafeAreaInsetsContext,
-  useSafeAreaInsets,
-  initialWindowMetrics,
 } from 'react-native-safe-area-context';
-import FontAwesome from 'react-native-vector-icons/FontAwesome';
-import Header from '../components/Header';
-import Footer from '../components/Footer';
+//import FontAwesome from 'react-native-vector-icons/FontAwesome';
+//import Footer from '../components/Footer';
 // import { useDispatch, useSelector } from 'react-redux';
 // import {  } from '../reducers/user';
 
@@ -31,56 +25,190 @@ export default function HomeScreen({ navigation }) {
   const handleServiceProviderSignUp = () => {
       navigation.navigate('ServiceProviderSignUp');
   };
-  const handleConnection = () => {
-    navigation.navigate('TabNavigator');
-  };
   
   return (
       <SafeAreaView style={styles.container}>
+          <Image source={require('../assets/Logo-PA.png')} style={styles.logoImage} />
+
         <View style={styles.haut}>
+          <View style={styles.buttonContainer}>
+          <LinearGradient
+            colors={['#CD43FD', '#FF7A00', '#FAB26F', '#FFE279']}
+            start={{ x: 0.0, y: 1.0 }} end={{ x: 1.0, y: 1.0 }}
+            style={styles.headers}>
+            <TouchableOpacity style={styles.button} onPress={() => handleOwnerSignUp()}>
+                <Text style={styles.buttonText}>Inscription Propriétaire</Text>
+              </TouchableOpacity>
+              </LinearGradient>
+              <LinearGradient
+            colors={['#CD43FD', '#FF7A00', '#FAB26F', '#FFE279']}
+            start={{ x: 0.0, y: 1.0 }} end={{ x: 1.0, y: 1.0 }}
+            style={styles.headers}>
+              <TouchableOpacity style={styles.button} onPress={() => handleServiceProviderSignUp()}>
+                <Text style={styles.buttonText}>Inscription Prestataire</Text>
+              </TouchableOpacity>
+              </LinearGradient>
+          </View>
+          <Text style={styles.text}>Se connecter :</Text>
           <View style={styles.bider}>
-            <Text>//Screen HomeScreen === Page d'accueil avant connexion//</Text>
-            <TouchableOpacity onPress={() => handleOwnerSignUp()} style={styles.button} activeOpacity={0.8}>
-                <Text style={styles.textButton}>SignUp Propriétaire</Text>
+            <TouchableOpacity onPress={() => handleConnection()} style={styles.signUpButtonFb} activeOpacity={0.8}>
+            <Image source={require('../assets/Logo-fb.png')} style={styles.logoFb} />
+                <Text style={styles.textButton}>Via Facebook</Text>
             </TouchableOpacity>
-            <TouchableOpacity onPress={() => handleServiceProviderSignUp()} style={styles.button} activeOpacity={0.8}>
-                <Text style={styles.textButton}>SignUp Prestataire</Text>
+            <TouchableOpacity onPress={() => handleConnection()} style={styles.signUpButtonGoogle} activeOpacity={0.8}>
+            <Image source={require('../assets/Logo-google.png')} style={styles.logoGoogle} />
+                <Text style={styles.textButton}>Via Google</Text>
             </TouchableOpacity>
-            <TouchableOpacity onPress={() => handleConnection()} style={styles.button} activeOpacity={0.8}>
+            <TouchableOpacity onPress={() => handleConnection()} style={styles.signUpButtonEmail} activeOpacity={0.8}>
+              <Image source={require('../assets/Logo-email.png')} style={styles.logoEmail} />
+                <Text style={styles.textButton}>Via email</Text>
+            </TouchableOpacity>
+            <TouchableOpacity onPress={() => handleConnection()} style={styles.buttons} activeOpacity={0.8}>
                 <Text style={styles.textButton}>(//après connection//) Mes hébergements</Text>
             </TouchableOpacity>
-            <TouchableOpacity onPress={() => navigation.navigate('Message')} style={styles.button} activeOpacity={0.8}>
+            <TouchableOpacity onPress={() => navigation.navigate('Message')} style={styles.buttons} activeOpacity={0.8}>
                 <Text style={styles.textButton}>(//Vers la Messagerie//)</Text>
             </TouchableOpacity>
           </View>
         </View>
-        <Footer navigation={navigation} messageButton={false}/>
       </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
+  bider :{
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 30,
+  },
+  button: {
+    width: 168,
+    height: 64,
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    backgroundColor: 'white',
+    borderRadius: 10,
+    borderRadius: 10,
+    margin: 2,
+    justifyContent: 'center',
+  },
+  buttonContainer: {
+    display: 'flex',
+    width: '100%',
+    height: 41,
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  buttonText: {
+    color: 'black',
+    fontWeight: 'bold',
+    fontSize: 14,
+    marginBottom: 5,
+  },
   container: {
     flex: 1,
     alignItems: 'center',
-    justifyContent: 'flex-start',
-    marginTop: Platform.OS === "android" ? 37 : 0,
-    backgroundColor: '#DDD',
-    borderWidth: 1,
-    borderColor: 'red',
+    backgroundColor: 'white',
+    paddingTop: 40,
   },
-  button: {
+  headers: {
+    width: 172,
+    height: 68,
+    borderRadius: 10,
+    borderRadius: 10,
+    marginTop: -190,
+    marginRight: 9,
+    marginLeft: 9,
+  },
+  logoEmail:{
+    width: '20%',
+    height: '60%',
+    marginLeft: -105,
+  },
+  logoFb:{
+    width: '15%',
+    height: '80%',
+    marginLeft: -87,
+  },
+  logoGoogle:{
+    width: '15%',
+    height: '80%',
+    marginLeft: -87,
+  },
+  logoImage:{
     alignItems: 'center',
-    paddingTop: 8,
-    width: '100%',
-    marginTop: 30,
-    backgroundColor: '#fbe29c',
-    borderRadius: 1,
+    marginTop: -160,
+   },
+  signUpButtonEmail: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#d4d4d4',
+    width: 280,
+    height: 50,
+    borderRadius: 10,
+    marginBottom: 15,
+    borderWidth: 1,
+    borderColor: '#a6a6a6',
+    shadowColor: '#ffe09',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.20,
+    shadowRadius: 6.41,
+    elevation: 4,
+  },
+  signUpButtonFb: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#3B589E',
+    width: 280,
+    height: 50,
+    borderRadius: 10,
+    marginBottom: 15,
+    shadowColor: '#ffe09',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.20,
+    shadowRadius: 6.41,
+    elevation: 4,
+  },
+  signUpButtonGoogle: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#DC4B38',
+    width: 280,
+    height: 50,
+    borderRadius: 10,
+    marginBottom: 15,
+    shadowColor: '#ffe09',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.20,
+    shadowRadius: 6.41,
+    elevation: 4,
+  },
+  text: {
+    fontSize: 18,
+    alignSelf: 'center',
   },
   textButton: {
-    //fontFamily: 'Futura',
-    height: 30,
-    fontWeight: '600',
+    color: 'white',
     fontSize: 16,
+    alignSelf: 'center',
+    marginLeft: 50,
   },
 });
